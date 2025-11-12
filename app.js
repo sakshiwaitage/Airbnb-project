d55962aa66b9919b0615452a901f53b06d0fc17e
Sakshi Waitage <sakshiwaitage245@gmail.com>
2025-11-12 13:04:02 +0530

    changed some files for better mobile responsiveness

--- app.js
+++ app.js
@@ -70,6 +70,10 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currUser = req.user;
+  next();
+});
 passport.use(new localStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
